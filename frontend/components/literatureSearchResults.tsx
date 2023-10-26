3c42ecc682440c7c58f54a909b297556e517b951
Carlo Beckmann <business@carlo-beckmann.com>
2023-10-26 17:45:04 +0200

    clean up components

--- frontend/components/literatureSearchResults.tsx
+++ frontend/components/literatureSearchResults.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-//import { fetchSearchData } from "@/app/api/fetchSearchData";
 import { getClient } from "@/lib/client";
 import { ApolloWrapper } from "@/lib/apollo-wrapper";
 import { GetSearchResultsDocument } from "@/graphql/queries/GetSearchResults.generated";
@@ -13,14 +12,10 @@ export default async function LiteratureSearchResults({
   query,
   offset,
 }: Props) {
-  //const data = await fetchSearchData(query, offset);
   const data = await getClient().query({
     query: GetSearchResultsDocument,
     variables: { query: query },
   });
 
-  
-
-  //return <div>{JSON.stringify(data, undefined, 2)}</div>;
   return <ApolloWrapper>{JSON.stringify(data.data.characters?.results, undefined, 2)}</ApolloWrapper>;
 }
