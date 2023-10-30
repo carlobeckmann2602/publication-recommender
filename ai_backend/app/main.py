33347e46a7fca8f07f025b860406c608f4b30e17
Jakob Tanz <jakob.tanz@study.hs-duesseldorf.de>
2023-10-31 00:18:37 +0100

    Initial local Container Setup

--- ai_backend/app/main.py
+++ ai_backend/app/main.py
@@ -3,25 +3,25 @@ from typing import List, Dict
 from .engine import Summarizer, Recommender
 import os
 
-rec_app = FastAPI()
+rec_api = FastAPI()
 initial_engines: Dict[str, Recommender] = {}
-rec_app.engines = initial_engines
+rec_api.engines = initial_engines
 
-rec_app.model_path = "./ai_backend/data/generated_data/"
+rec_api.model_path = "../data/generated_data/"
 
 
-@rec_app.get("/models/possible")
+@rec_api.get("/models/possible")
 def get_model_names() -> Dict[str, List[str]]:
     """
     Lists all registered annoy models, that can be loaded from disk
 
     **return**: a list of possible model names
     """
-    models = next(os.walk(rec_app.model_path))[1]
+    models = next(os.walk(rec_api.model_path))[1]
     return {"disk_models": models}
 
 
-@rec_app.get("/{model_name}/load/")
+@rec_api.get("/{model_name}/load/")
 def load_recommender(model_name: str,
                      token_amount: int = 5,
                      annoy_input_length: int = 768,
@@ -42,12 +42,12 @@ def load_recommender(model_name: str,
                               token_amount=token_amount,
                               annoy_input_length=annoy_input_length,
                               annoy_n_trees=annoy_n_trees)
-    recommender.load(path=rec_app.model_path, model_name=model_name)
-    rec_app.engines[model_name] = recommender
+    recommender.load(path=rec_api.model_path, model_name=model_name)
+    rec_api.engines[model_name] = recommender
     return {"loaded": model_name}
 
 
-@rec_app.get("/{model_name}/match_id/{publication_id}/")
+@rec_api.get("/{model_name}/match_id/{publication_id}/")
 async def get_recommendation(model_name: str, publication_id: str, amount: int = 5):
     """
     Runs the recommendation engine for a publication ID.
@@ -57,16 +57,16 @@ async def get_recommendation(model_name: str, publication_id: str, amount: int =
 
     **return**: The found matches plus additional information like the used tokens, the distances and anny indexes
     """
-    if model_name not in rec_app.engines.keys():
+    if model_name not in rec_api.engines.keys():
         raise HTTPException(status_code=404, detail="No model with this name loaded")
-    recommender = rec_app.engines[model_name]
+    recommender = rec_api.engines[model_name]
     if publication_id not in recommender.mapping[Recommender.PUB_ID_KEY].values:
         raise HTTPException(status_code=404, detail="No Publication with this ID in mapping")
     matches = recommender.get_match_by_id(str(publication_id), amount)
     return matches.to_dict()
 
 
-@rec_app.get("/{model_name}/match_token/{token}/")
+@rec_api.get("/{model_name}/match_token/{token}/")
 async def get_recommendation(model_name: str, token: str, amount: int = 5):
     """
     Runs the recommendation engine for a publication ID.
@@ -76,14 +76,14 @@ async def get_recommendation(model_name: str, token: str, amount: int = 5):
 
     **return**: The found matches plus additional information like the used tokens, the distances and anny indexes
     """
-    if model_name not in rec_app.engines.keys():
+    if model_name not in rec_api.engines.keys():
         raise HTTPException(status_code=404, detail="No model with this name loaded")
-    recommender = rec_app.engines[model_name]
+    recommender = rec_api.engines[model_name]
     matches = recommender.get_match_by_token(str(token), amount)
     return matches.to_dict()
 
 
-@rec_app.get("/{model_name}/random_id/")
+@rec_api.get("/{model_name}/random_id/")
 async def get_random(model_name: str):
     """
     Selects a random publication ID that is included in the mapping of the selected model
@@ -91,17 +91,17 @@ async def get_random(model_name: str):
 
     **return**: A random publication
     """
-    if model_name not in rec_app.engines.keys():
+    if model_name not in rec_api.engines.keys():
         raise HTTPException(status_code=404, detail="No model with this name loaded")
-    publication = rec_app.engines[model_name].mapping.sample()[[Recommender.PUB_ID_KEY, Recommender.ANNOY_INDEX_KEY]]
+    publication = rec_api.engines[model_name].mapping.sample()[[Recommender.PUB_ID_KEY, Recommender.ANNOY_INDEX_KEY]]
     return publication.to_dict()
 
 
-@rec_app.get("/models/loaded/")
+@rec_api.get("/models/loaded/")
 async def get_current_recommender():
     """
     Returns all the names of the currently loaded models
 
     **return**: The current model (recommendation engine)
     """
-    return {"current_model": str(list(rec_app.engines.keys()))}
+    return {"current_model": str(list(rec_api.engines.keys()))}
