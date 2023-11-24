97e9420f3f2ad77fede244b585444ef923f2f28c
Carlo Beckmann <business@carlo-beckmann.com>
2023-11-24 14:50:40 +0100

    fix: use api with no-cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'PUT, GET, POST, OPTIONS, PATCH, DELETE',
+    credentials: true,
+  });
   await app.listen(3000);
 }
-bootstrap();
+bootstrap().then();
--- frontend/constants/urls.ts
+++ frontend/constants/urls.ts
@@ -1,1 +1,2 @@
-export const GRAPHQL_URL = process.env.NEXT_PUBLIC_GRAPHQL_URL;
+export const GRAPHQL_URL =
+  process.env.NEXT_PUBLIC_CLIENT_BACKEND_GRAPHQL_ENDPOINT;
