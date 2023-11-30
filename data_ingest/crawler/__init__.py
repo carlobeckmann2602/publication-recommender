c40208fcaf597efdec5428065a2093acb36e76e0
Julian Bringezu <v.bringezu@gmail.com>
2023-11-30 23:31:31 +0100

    path refactoring

--- data_ingest/crawler/__init__.py
+++
@@ -1,3 +0,0 @@
-from .arxiv_crawler import ArxivCrawler
-from .arxiv_api_crawler import ArxivApiCrawler
-from .pdf_crawler import PdfCrawler
--- data_ingest/data_ingest.py
+++
@@ -1,5 +0,0 @@
-from crawler import ArxivApiCrawler
-
-if __name__ == '__main__':
-    ac = ArxivApiCrawler()
-    ac.run()
