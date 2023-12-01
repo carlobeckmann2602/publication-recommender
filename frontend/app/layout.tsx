1f788e781f659f08fcc0abf68a3e725c8064337d
Carlo Beckmann <business@carlo-beckmann.com>
2023-12-01 12:06:26 +0100

    style: add logo to navbar

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -8,7 +8,7 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Publicationrcmd",
+  title: "Publicationrecmd",
   description: "Find your academic publication",
 };
 
