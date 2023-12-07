75f323e4245241edf699f7dfcdc489f8bbdbd56f
Carlo Beckmann <business@carlo-beckmann.com>
2023-12-07 15:11:04 +0100

    fix: remove hardcoded publications backend results

--- backend/src/modules/core/publication/services/publication.service.ts
+++ backend/src/modules/core/publication/services/publication.service.ts
@@ -20,22 +20,9 @@ export class PublicationService {
   async findAll(query: string): Promise<Publication[]> {
     let parsedResult: AnnoyResultDto;
     try {
-      //await fetch('http://ai_backend:8000/arxiv_6k-v2/load');
-      //const result = await (await fetch(`http://ai_backend:8000/arxiv_6k-v2/match_token/${query}`)).json();
-      const result = {
-        matches: [
-          { id: '82cd0b62-ba19-4151-874d-518ed5aeb2a2' },
-          { id: '8f5fc827-0d29-4d94-a3d6-9a08ffe1435d' },
-          { id: 'a184eff2-de72-406b-b62e-060633dd5663' },
-          { id: '10abc56d-1fbe-41b2-b862-e15ba37e8d42' },
-          { id: 'b18bf642-80aa-407f-a978-06e219044d2e' },
-          { id: '69a940c1-e1fa-4eb5-97d0-1bad66b77373' },
-          { id: '84aed547-e3c4-4199-a977-69d9d15d6eba' },
-          { id: '3640b883-1d67-4977-8cf7-2b74a1dd9cee' },
-          { id: 'cc4fc692-a0d9-47d7-a5d9-8d71350e2de4' },
-          { id: '52de13c2-4a58-4411-8d00-358fec60efaf' },
-        ],
-      };
+      await fetch('http://ai_backend:8000/arxiv_6k-v2/load');
+      const result = await (await fetch(`http://ai_backend:8000/arxiv_6k-v2/match_token/${query}`)).json();
+
       parsedResult = plainToClass(AnnoyResultDto, result);
       await validateOrReject(parsedResult);
     } catch (e) {
