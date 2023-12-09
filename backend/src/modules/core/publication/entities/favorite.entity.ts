24eafae5edf2a4de896c4c70b9f9d5aa1fe4e50a
Robin <robin.steil@gmx.de>
2023-12-09 01:36:35 +0100

    Sync code with db

--- backend/src/modules/core/publication/entities/favorite.entity.ts
+++ backend/src/modules/core/publication/entities/favorite.entity.ts
@@ -2,31 +2,36 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  Index,
   JoinColumn,
   ManyToOne,
   PrimaryGeneratedColumn,
+  Unique,
   UpdateDateColumn,
 } from 'typeorm';
 import { User } from '../../user/entities/user.entity';
 import { Publication } from './publication.entity';
 
 @Entity('favorites')
+@Unique('favorites_user_id_publication_id_key', ['user_id', 'publication_id'])
 export class Favorite {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
+  @Index('favorites_user_id_idx')
   user_id: string;
 
   @Column()
+  @Index('favorites_publication_id_idx')
   publication_id: string;
 
-  @ManyToOne(() => User)
-  @JoinColumn({ name: 'user_id' })
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'user_id', foreignKeyConstraintName: 'favorites_user_id_fkey' })
   user: User;
 
-  @ManyToOne(() => Publication)
-  @JoinColumn({ name: 'publication_id' })
+  @ManyToOne(() => Publication, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'publication_id', foreignKeyConstraintName: 'favorites_publication_id_fkey' })
   publication: Publication;
 
   @CreateDateColumn({ name: 'created_at' })
