f02d43e169769906ed2db8dc16fa80c8e0eb41fa
Carlo Beckmann <business@carlo-beckmann.com>
2024-01-11 22:22:51 +0100

    feat: add update user forms to profile page

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,9 +1,19 @@
 import { Header } from "@/components/Header";
+import DeleteAccountButton from "@/components/profile/DeleteAccountButton";
+import PasswordForm from "@/components/profile/PasswordForm";
+import UserCredentialForm from "@/components/profile/UserCredentialForm";
 
 export default function Profile() {
   return (
-    <div>
-      <Header title="Profile" />
-    </div>
+    <>
+      <Header title="Profile" subtitle="your personal information" />
+      <div className="flex justify-center p-8">
+        <div className="flex flex-col w-full sm:w-3/4 lg:w-1/2 gap-8">
+          <UserCredentialForm />
+          <PasswordForm />
+          <DeleteAccountButton />
+        </div>
+      </div>
+    </>
   );
 }
