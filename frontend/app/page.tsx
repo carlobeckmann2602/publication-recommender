1858bf4fe295d67e2f626d32bc15ba7ffe69f509
Carlo Beckmann <business@carlo-beckmann.com>
2024-01-11 16:53:12 +0100

    feat: add newest recommendation slider

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,17 @@
-import RecommendationSlider from "@/components/recommendation/RecommendationSlider";
+import NewestRecommendationSlider from "@/components/recommendation/NewestRecommendationSlider";
 import { Searchbar } from "@/components/search/Searchbar";
 
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center h-full items-center flex-col py-4">
+      <div className="flex justify-center h-full items-center flex-col py-4 gap-8">
         <Searchbar />
+        <div className="w-full">
+          <NewestRecommendationSlider
+            title="Your newest Recommendation"
+            titleClassName="text-center"
+          />
+        </div>
       </div>
     </>
   );
