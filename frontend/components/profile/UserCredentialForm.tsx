439183888c192000ed179694f210597f58413aad
Carlo Beckmann <business@carlo-beckmann.com>
2024-01-11 22:21:13 +0100

    feat: create update user credential form

---
+++ frontend/components/profile/UserCredentialForm.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import { UpdateUserDocument } from "@/graphql/mutation/UpdateUser.generated";
+import { useMutation } from "@apollo/client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
+import { AlertTriangle } from "lucide-react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+
+const FormSchema = z.object({
+  name: z
+    .string({
+      required_error: "Name is required",
+    })
+    .min(2, {
+      message: "Name must be at least 2 characters.",
+    }),
+  email: z
+    .string({
+      required_error: "Name is required",
+    })
+    .email({ message: "Invalid email address" }),
+});
+
+export default function UserCredentialForm() {
+  const session = useSession();
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: session.data?.user.name,
+      email: session.data?.user.email,
+    },
+  });
+
+  const [errorMsg, setErrorMsg] = useState<string>();
+
+  const [updateUser, { error }] = useMutation(UpdateUserDocument, {
+    context: {
+      headers: {
+        Authorization: `Bearer ${session.data?.userToken.jwtToken}`,
+      },
+    },
+  });
+
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+    try {
+      const response = await updateUser({
+        variables: {
+          email: data.email,
+          name: data.name,
+        },
+      });
+      if (response.errors) {
+        throw new Error(response.errors.toString());
+      } else {
+      }
+    } catch (error: any) {
+      console.error(error.message);
+      setErrorMsg("Update Credantial failed!");
+    }
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Name"
+                    type="text"
+                    autoComplete="on"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Email"
+                    type="email"
+                    autoComplete="on"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="w-full">
+            Update
+          </Button>
+        </form>
+      </Form>
+      {!!errorMsg && (
+        <Alert variant="destructive">
+          <AlertTriangle className="h-4 w-4" />
+          <AlertTitle>Error</AlertTitle>
+          <AlertDescription>{errorMsg}</AlertDescription>
+        </Alert>
+      )}
+    </>
+  );
+}
